39ee25902eec25fca998b0cd740677c69935c837
Mako-Ito <otyazuki0818owl@gmail.com>
2025-12-03 23:39:08 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ export default function App() {
   // 各画像にダウンロードURLを設定できます
   const productImages: ProductImage[] = [
     {
-      url: "https://dl.dropboxusercontent.com/scl/fi/bp2x1wz7elsr75o87df8x/ITO_Tokuharu_Profile.JPG?rlkey=l7ldx1z0ibys109jima7mzi8l",
+      url: "https://raw.githubusercontent.com/Mako-Ito/gallery/main/public/ITO_Tokuharu_Profile.JPG",
       downloadUrl: "https://www.dropbox.com/scl/fi/bp2x1wz7elsr75o87df8x/ITO_Tokuharu_Profile.JPG?dl=1", // ここにダウンロードリンクを設定
       alt: "商品画像1",
     },
     {
-      url: "https://dl.dropboxusercontent.com/scl/fi/kr5zd3tea1cjal3qab423/IMG_0026.JPG?rlkey=1yz9jh7kj2y6d38g2u599r5hm",
+      url: "https://raw.githubusercontent.com/Mako-Ito/gallery/main/public/IMG_0026.JPG",
       downloadUrl: "https://www.dropbox.com/scl/fi/kr5zd3tea1cjal3qab423/IMG_0026.JPG?dl=1", // ここにダウンロードリンクを設定
       alt: "商品画像2",
     },
